43120d2922c626cfff88e52e0e2b90cf3afa1792
samuelemezzasalma <samuele.mezza@gmail.com>
2022-11-01 19:40:27 +0100

    2 files modified, 2 added included css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,31 @@
 	<meta name="description" content="Personal page" />
 </svelte:head>
 
+<div class="g-cont">
+<section class="img_profile">
+	<img class="profile" src="https://avatars.githubusercontent.com/u/4698607?v=4" alt="profile picture">
+</section>
+<section>
+	<h1>Hello I'm Samuele Mezzasalma</h1>
+	<p>I’m a developer based in the Italy</p>
+	<div>
+		<ul>
+			<li>Twitter</li>
+			<li>Youtube</li>
+			<li>Github</li>
+			<li>Linkedin</li>
+		</ul>	
+	</div>
+</section>
 
+</div>
 
 
 
 <style>
+	.profile {
+		border-radius: 50%;
+		width: 100%;
+	}
+
 </style>
