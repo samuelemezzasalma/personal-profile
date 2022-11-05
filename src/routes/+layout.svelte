5898029c601327355b22e4ed269d2d42fdb937f5
samuelemezzasalma <samuele.mezza@gmail.com>
2022-11-05 15:02:57 +0100

    fixed html and started a new css based on grid

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,32 +3,38 @@
 	import './styles2.css';
 </script>
 
-<div class="app">
+<div class="g-cont app">
 
-	<header class="g-cont">
-	<div class="header-left-block g-item"></div>
-		<nav id="navbar" class="g-item">
-			
-			<input id="dropdown" class="input-box" type="checkbox" >
-			<label for="dropdown" class="dropdown">
-				<span class="icon" aria-hidden="true">A</span>
-			</label>
-			<ul class="menu-list">
-				<li><a href="#about">About</a></li>
-				<li><a href="#speaking">Speaking</a></li>
-				<li><a href="#writing">Writing</a></li>
-				<li><a href="#uses">Uses</a></li>
-				<li><a href="#contact">Contact</a></li>
-				<li><a href="#dark-mode">Dark mode</a></li>
-			</ul>
-		</nav>
+	<header class="g-cont header">
+		<div class="g-item header-icon ">
+			<p>Test</p>
+		</div>
+		<div class="g-item header-title ">
+			<p>Test</p>
+		</div>
+		<div class="g-item header-menu">
+			<nav id="navbar">
+				<input id="dropdown" class="input-box" type="checkbox" >
+				<label for="dropdown" class="burger">
+					<span class="icon" aria-hidden="true">A</span>
+				</label>
+				<ul class="menu-list">
+					<li><a href="#about">About</a></li>
+					<li><a href="#speaking">Speaking</a></li>
+					<li><a href="#writing">Writing</a></li>
+					<li><a href="#uses">Uses</a></li>
+					<li><a href="#contact">Contact</a></li>
+					<li><a href="#dark-mode">Dark mode</a></li>
+				</ul>
+			</nav>
+		</div>
 	</header>
 
-	<main>
+	<main class="g-cont main-container">
 		<slot />
 	</main>
 
-	<footer>
+	<footer class="g-cont footer">
 		<section>
 			<h2>Get in touch</h2>
 			<ul>
